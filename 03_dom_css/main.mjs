bde284c616b48c7a153407b6f386253681504187
minlwin <lwin.zawmin@gmail.com>
2019-12-07 13:08:21 +0630

    Commit Add and Search

--- 03_dom_css/main.mjs
+++ 03_dom_css/main.mjs
@@ -1,20 +1,79 @@
+import { Patient } from "./modules/patients-model.mjs"
+import { MODEL } from "./modules/patients-model.mjs"
+
 function search() {
 
+    $('#result').empty()
+
+    let list = MODEL.search($('#sch-gender').val(), $('#sch-name').val(), $('#sch-phone').val())
+
+    for(let p of list) {
+        $('#result').append(getTableRow(p))
+    }
+
+    $('.edit-link').on('click', edit)
 }
 
 function addNew() {
-    alert("Add New")
+    $('#edit-title').text('Add New Patient')
+    $('#modal-add').modal('show')
+}
+
+function edit(event) {
+    let id = $(event.target).text()
+
+    let patient = MODEL.findById(id)
+
+    console.log(patient)
+    $('#name').val(patient.name)
+
+    $('#edit-title').text('Edit Patient')
+    $('#modal-add').modal('show')
+}
+
+function getTableRow(patient) {
+    let row = $('<tr>')
+    $(row).append($('<td>').append(
+        $('<a>').text(patient['id'])
+            .attr("href", "#")
+            .attr('class', 'edit-link')
+    ))
+    $(row).append($('<td>').text(patient['name']))
+    $(row).append($('<td>').text(patient['gender']))
+    $(row).append($('<td>').text(patient['dob']))
+    $(row).append($('<td>').text(patient['phone']))
+    $(row).append($('<td>').text(patient['address']))
+    return row
 }
 
 function save() {
 
+    let name = $('#name').val()
+    let gender = $('#male').select() ? 'Male' : 'Female'
+    let dob = $('#dob').val()
+    let phone = $('#phone').val()
+    let address = $('#address').val()
+
+    let patient = new Patient(name, gender, dob, phone, address)
+
+    MODEL.save(patient)
+    clearInputs()
+    $('#modal-add').modal('hide')
+}
+
+function clearInputs() {
+    $('#name').val("")
+    $('#male').prop('check', true)
+    $('#dob').val("")
+    $('#phone').val("")
+    $('#address').val("")
 }
 
 window.onload = () => {
 
     document.getElementById('searchBtn')
-        .addEventListener('click', search())
-
+        .addEventListener('click', search)
+    document.getElementById('saveBtn').addEventListener('click', save)
     document.getElementById('addBtn').addEventListener('click', addNew)
 }
 
--- 03_dom_css/modules/patients-model.mjs
+++ 03_dom_css/modules/patients-model.mjs
@@ -1,12 +1,23 @@
 const STORAGE = 'com.jdc.patients'
 
-export function Patient(id, name, gender, dob, phone, address) {
-    this.id = id
+export function Patient(name, gender, dob, phone, address) {
     this.name = name
     this.gender = gender
     this.dob = dob
     this.phone = phone
     this.address = address
+}
+
+const PATIENT_ID_KEY = "com.jdc.patient.id"
+
+let IdGenerator = {
+
+    generate() {
+        let id = localStorage.getItem(PATIENT_ID_KEY)
+        id = id ? Number.parseInt(id) + 1 : 1
+        localStorage.setItem(PATIENT_ID_KEY, id)
+        return id
+    }
 }
 
 class PatientModel {
@@ -19,15 +30,16 @@ class PatientModel {
     search(gender, name, phone) {
         return this.patients.filter(p => {
             
-            if(gender != '0' && gender != p.gender) {
-                return false
+            if(gender != '0') {
+                let schGender = gender == '1' ? 'Male' : 'Female'
+                return schGender == p.gender
             } 
 
-            if(name && !p.name.toLowerCase().startWith(name.toLowerCase())) {
+            if(name && !p.name.toLowerCase().startsWith(name.toLowerCase())) {
                 return false
             }
 
-            if(phone && !p.phone.startWith(phone)) {
+            if(phone && !p.phone.startsWith(phone)) {
                 return false
             }
 
@@ -35,11 +47,34 @@ class PatientModel {
         })
     }
 
-    addNew(patient) {
-        this.patients.push(patient)
+    findById(id) {
+
+        for(let p of this.patients) {
+            if(p.id == id) {
+                return p
+            }
+        }
+    }
+
+    save(patient) {
+
+        if(patient.id) {
+
+            for(let i = 0; i < this.patients.length; i ++) {
+                if(patient.id == this.patients[i].id) {
+                    this.patients[i] = patient
+                }
+            }
+
+        } else {
+            patient.id = IdGenerator.generate()
+            this.patients.push(patient)
+        }
+
         let str = JSON.stringify(this.patients)
-        localStorage.setItem(STORAGE, str)
+        localStorage.setItem(STORAGE, str)            
     }
 }
 
 export const MODEL = new PatientModel
+
